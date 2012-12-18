333d0d37f2d011db65820de55961708691aa48fc
ernestoSevilla <ernestoabram@gmail.com>
2012-12-18 16:54:42 +0100

    primera subida

---
+++ UnaNuevaPruebaDeGEdit/src/com/proin/unaNuevaPruebaGEDIT/naNuevaPruebaGEDIT.java
@@ -0,0 +1,13 @@
+package com.proin.unaNuevaPruebaGEDIT;
+
+public class naNuevaPruebaGEDIT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(" Hola git ");
+
+	}
+
+}
