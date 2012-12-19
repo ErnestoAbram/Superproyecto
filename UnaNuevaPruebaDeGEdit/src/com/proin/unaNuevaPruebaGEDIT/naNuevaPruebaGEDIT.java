843672944257e5276266b014048ede2b078bd027
ErnestoSevilla <ernestoabram@gmail.com>
2012-12-19 10:17:04 +0100

    Update UnaNuevaPruebaDeGEdit/src/com/proin/unaNuevaPruebaGEDIT/naNuevaPruebaGEDIT.java

--- UnaNuevaPruebaDeGEdit/src/com/proin/unaNuevaPruebaGEDIT/naNuevaPruebaGEDIT.java
+++ UnaNuevaPruebaDeGEdit/src/com/proin/unaNuevaPruebaGEDIT/naNuevaPruebaGEDIT.java
@@ -6,7 +6,7 @@ public class naNuevaPruebaGEDIT {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(" Hola git V2");
+		System.out.println(" Hola git V3");
 
 	}
 
